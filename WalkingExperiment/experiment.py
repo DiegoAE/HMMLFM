6e8b554e11d2b7f56e70d669ed54e529cd09cac3
diego <aleagues@gmail.com>
2016-09-19 07:45:57 -0500

    Real data experiment at the state of CIARP 2016 paper.

--- WalkingExperiment/experiment.py
+++ WalkingExperiment/experiment.py
@@ -36,13 +36,14 @@ locations_per_segment = data['lps']
 #
 
 number_lfm = 3
-number_latent_forces = 4
+number_latent_forces = 3
 start_t = 0.1
 end_t = 5.1
 model = LFMHMMcontinuousMO(outputs, number_lfm, locations_per_segment,
                              start_t, end_t, number_latent_forces, verbose=True)
 
 colors_cycle = ['red', 'green', 'blue', 'purple']
+joints_name = ['left tibia', 'right tibia', 'left radius', 'right radius']
 # Plotting the inputs
 # considered_idx = 0
 # last_value = 0
@@ -68,8 +69,8 @@ print model.LFMparams
 
 print "start training"
 
-train_flag = True
-file_name = "MO_MOCAP_4_forces_3_hs"
+train_flag = False
+file_name = "MO_MOCAP_3_forces"
 if train_flag:
     model.train()
     model.save_params("/home/diego/tmp/Parameters/WALKING", file_name)
@@ -96,7 +97,6 @@ print viterbi_testing
 
 # Looking at the resulting fit
 
-
 number_testing_points = 100
 
 considered_idx = 0
@@ -108,6 +108,8 @@ considered_segments = testing_observations[considered_idx].shape[0]
 # print considered_segments
 for i in xrange(considered_segments):
     c_hidden_state = regression_hidden_states[i]
+    plt.text(1 + i * 20 - i, .75, r'$z_{%d}=%d$' % (i, c_hidden_state),
+             fontsize=23)
     c_obv = testing_observations[considered_idx][i]
     # predicting more time steps
     t_test = np.linspace(start_t, end_t, number_testing_points)
@@ -131,36 +133,20 @@ for i in xrange(considered_segments):
             0, model.locations_per_segment - 1, model.locations_per_segment)
     for j in xrange(outputs):
         plt.scatter(obs_plotting_locations, c_obv[j::outputs],
-                    color=colors_cycle[j])
+                    color=colors_cycle[j], label=[None, joints_name[j]][i == 0])
     test_plotting_locations = last_value + np.linspace(
             0, model.locations_per_segment - 1, number_testing_points)
     for j in xrange(outputs):
         plt.plot(test_plotting_locations, current_outputs[:, j],
-                 color=colors_cycle[j], label=[None, 'predicted mean'][i == 0])
-    plt.plot(test_plotting_locations,
-             current_outputs - 2 * np.sqrt(current_covariances), 'k--')
-    plt.plot(test_plotting_locations,
-             current_outputs + 2 * np.sqrt(current_covariances), 'k--')
+                 color=colors_cycle[j])
+        lower_trajectory = current_outputs[:, j] -\
+                           2 * np.sqrt(current_covariances[:, j])
+        upper_trajectory = current_outputs[:, j] +\
+                           2 * np.sqrt(current_covariances[:, j])
+        plt.fill_between(test_plotting_locations, lower_trajectory,
+                         upper_trajectory, alpha=0.4, facecolor=colors_cycle[j])
     last_value = last_value + model.locations_per_segment - 1
     plt.axvline(x=last_value, color='red', linestyle='--')
+plt.legend(loc='lower left')
 plt.show()
 
-# # This is only useful for synthetic experiments.
-# def f(a):
-#     if a == 0:
-#         return 1
-#     if a == 1:
-#         return 0
-#     return 2
-#
-# TMP = data['training_viterbi']
-#
-# print "Training Vit"
-# for x in TMP:
-#     print map(f, x)
-#
-# TMP = data['testing_viterbi']
-#
-# print "Testing Vit"
-# for x in TMP:
-#     print map(f, x)

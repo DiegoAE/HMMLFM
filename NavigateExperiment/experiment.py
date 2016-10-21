a5157bc9014bf9a770030c2ea78859ee3e150d82
diego <aleagues@gmail.com>
2016-10-21 16:00:25 -0500

    Uploading the report of the results of the MOCAP navigate experiment along with the extended experiment script with nice plotting.

--- NavigateExperiment/experiment.py
+++ NavigateExperiment/experiment.py
@@ -68,7 +68,7 @@ print model.LFMparams
 
 print "start training"
 
-train_flag = True
+train_flag = False
 file_name = "MO_MOCAP_SUBJECT_41_3_forces_6_HS_normalized"
 if train_flag:
     model.train()
@@ -98,54 +98,65 @@ print viterbi_testing
 # Looking at the resulting fit
 
 number_testing_points = 100
-
-considered_idx = 0
-regression_hidden_states = viterbi_testing[considered_idx]
-# print regression_hidden_states
-last_value = 0
-plt.axvline(x=last_value, color='red', linestyle='--')
-considered_segments = testing_observations[considered_idx].shape[0]
-# print considered_segments
-for i in xrange(considered_segments):
-    c_hidden_state = regression_hidden_states[i]
-    plt.text(1 + i * 20 - i, .75, r'$z_{%d}=%d$' % (i, c_hidden_state),
-             fontsize=23)
-    c_obv = testing_observations[considered_idx][i]
-    # predicting more time steps
-    t_test = np.linspace(start_t, end_t, number_testing_points)
-    mean_pred, cov_pred = model.predict(t_test, c_hidden_state, c_obv)
-    mean_pred = mean_pred.flatten()
-    cov_pred = np.diag(cov_pred)
-
-    current_outputs = np.zeros((number_testing_points, outputs))
-    current_covariances = np.zeros((number_testing_points, outputs))
-    # separating the outputs accordingly.
-    for j in xrange(outputs):
-        current_outputs[:, j] = mean_pred[j::outputs]
-        current_covariances[:, j] = cov_pred[j::outputs]
-
-    # NOTE: there is an important distinction between sample locations
-    # for evaluation and for plotting because different spaces are being used.
-    # Particularly, in the plotting space each sample is a unit away from each
-    # other. On the other hand, evaluation locations depend on start_t and end_t
-
-    obs_plotting_locations = last_value + np.linspace(
-            0, model.locations_per_segment - 1, model.locations_per_segment)
-    for j in xrange(outputs):
-        plt.scatter(obs_plotting_locations, c_obv[j::outputs],
-                    color=colors_cycle[j], label=[None, joints_name[j]][i == 0])
-    test_plotting_locations = last_value + np.linspace(
-            0, model.locations_per_segment - 1, number_testing_points)
-    for j in xrange(outputs):
-        plt.plot(test_plotting_locations, current_outputs[:, j],
-                 color=colors_cycle[j])
-        lower_trajectory = current_outputs[:, j] -\
-                           2 * np.sqrt(current_covariances[:, j])
-        upper_trajectory = current_outputs[:, j] +\
-                           2 * np.sqrt(current_covariances[:, j])
-        plt.fill_between(test_plotting_locations, lower_trajectory,
-                         upper_trajectory, alpha=0.4, facecolor=colors_cycle[j])
-    last_value = last_value + model.locations_per_segment - 1
+plt.figure(1)
+for considered_idx in xrange(3):
+    plt.subplot(3, 1, considered_idx + 1)
+    last_value = 0
     plt.axvline(x=last_value, color='red', linestyle='--')
+    if considered_idx < np.size(training_observations):
+        considered_segments = training_observations[considered_idx].shape[0]
+        regression_hidden_states = viterbi_training[considered_idx]
+        current_observation = training_observations[considered_idx]
+    else:
+        considered_segments = testing_observations[
+            considered_idx - np.size(training_observations)].shape[0]
+        regression_hidden_states = viterbi_testing[
+            considered_idx - np.size(training_observations)]
+        current_observation = testing_observations[
+            considered_idx - np.size(training_observations)]
+    for i in xrange(considered_segments):
+        c_hidden_state = regression_hidden_states[i]
+        # plt.text(1 + i * 20 - i, .75, r'$z_{%d}=%d$' % (i, c_hidden_state),
+        #          fontsize=23)
+        c_obv = current_observation[i]
+        # predicting more time steps
+        t_test = np.linspace(start_t, end_t, number_testing_points)
+        mean_pred, cov_pred = model.predict(t_test, c_hidden_state, c_obv)
+        mean_pred = mean_pred.flatten()
+        cov_pred = np.diag(cov_pred)
+
+        current_outputs = np.zeros((number_testing_points, outputs))
+        current_covariances = np.zeros((number_testing_points, outputs))
+        # separating the outputs accordingly.
+        for j in xrange(outputs):
+            current_outputs[:, j] = mean_pred[j::outputs]
+            current_covariances[:, j] = cov_pred[j::outputs]
+
+        # NOTE: there is an important distinction between sample locations
+        # for evaluation and for plotting because different spaces are being
+        # used. Particularly, in the plotting space each sample is a unit away
+        # from each other. On the other hand, evaluation locations depend on
+        # start_t and end_t
+
+        obs_plotting_locations = last_value + np.linspace(
+                0, model.locations_per_segment - 1, model.locations_per_segment)
+        for j in xrange(outputs):
+            plt.scatter(obs_plotting_locations, c_obv[j::outputs],
+                        color=colors_cycle[j],
+                        label=[None, joints_name[j]][i == 0])
+        test_plotting_locations = last_value + np.linspace(
+                0, model.locations_per_segment - 1, number_testing_points)
+        for j in xrange(outputs):
+            plt.plot(test_plotting_locations, current_outputs[:, j],
+                     color=colors_cycle[j])
+            lower_trajectory = current_outputs[:, j] - \
+                               2 * np.sqrt(current_covariances[:, j])
+            upper_trajectory = current_outputs[:, j] + \
+                               2 * np.sqrt(current_covariances[:, j])
+            plt.fill_between(test_plotting_locations, lower_trajectory,
+                             upper_trajectory, alpha=0.4,
+                             facecolor=colors_cycle[j])
+        last_value = last_value + model.locations_per_segment - 1
+        plt.axvline(x=last_value, color='red', linestyle='--')
 plt.legend(loc='lower left')
 plt.show()
